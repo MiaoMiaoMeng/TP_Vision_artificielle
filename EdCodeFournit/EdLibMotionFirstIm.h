231fbc7c2e43617f562427123600c64b6ed226a3
MiaoMiaoMeng <billknightms@gmail.com>
2019-11-22 17:46:06 +0100

    finish

--- EdCodeFournit/EdLibMotionFirstIm.h
+++ EdCodeFournit/EdLibMotionFirstIm.h
@@ -1,2 +1,3 @@
 #pragma once
 int MotionDetectionRefFirstIm(EdIMAGE *image, EdIMAGE *imres, EdIMAGE *imref, unsigned char seuil);
+int MotionDetectionRefresh(EdIMAGE *image, EdIMAGE *imres, EdIMAGE *imref, unsigned char Seuil , float Oublie);
